cacfe37cdb924e472cf1b100d1aa2108213448e3
Alejandro Martinez Aguirre <almarag@gmail.com>
2015-07-04 16:14:35 -0500

    Fixed createUser HTTP Method from GET to PUT

--- app/routes.php
+++ app/routes.php
@@ -20,7 +20,7 @@ Route::group(array('prefix' => 'api/v1'), function()
 {
     Route::get('info/{id}', ['uses' => 'ApiController@info']);
     Route::put('changePassword', ['uses' => 'ApiController@changePassword']);
-	Route::get('createUser', ['uses' => 'ApiController@createUser']);
+	Route::put('createUser', ['uses' => 'ApiController@createUser']);
 	Route::delete('deleteUser', ['uses' => 'ApiController@deleteUser']);
     Route::put('updateUser', ['uses' => 'ApiController@updateUser']);
     Route::post('authenticate', ['uses' => 'ApiController@authenticate']);
